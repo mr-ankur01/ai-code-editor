89cd84275ba0ddf6a6db3b9d11abac8e57c1e4d1
Ankur Sawant <ankursawant811@gmail.com>
2025-07-19 08:48:56 +0000

    web project output title show be showing like web output

--- src/app/editor/components/WebEditor.tsx
+++ src/app/editor/components/WebEditor.tsx
@@ -27,7 +27,7 @@ export function WebEditor({ setEditorCode, html, css, js, activeTab, onTabChange
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Web Project Output</title>
+        <title>Website Output</title>
         <style>${css}</style>
       </head>
       <body>
