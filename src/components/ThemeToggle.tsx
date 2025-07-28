29b43ac8890ebd31f66b8b01f78c070fe830cda4
Ankur Sawant <ankursawant811@gmail.com>
2025-07-28 15:14:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -18,10 +18,7 @@ export function ThemeToggle() {
   
   if (!mounted) {
     return (
-      <Button variant="ghost" size="icon" disabled={true} aria-label="Toggle theme">
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      </Button>
+      <Button variant="ghost" size="icon" disabled={true} aria-label="Toggle theme" />
     )
   }
 
