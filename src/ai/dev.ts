cf4984526a97fdbf73e616f63442ac149eba870e
Ankur Sawant <ankursawant811@gmail.com>
2025-07-30 10:07:31 +0000

    please clean up project by deleting unnecessary files.

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -5,6 +5,5 @@ import '@/ai/flows/chat-with-ai-assistant.ts';
 import '@/ai/flows/explain-code-selection.ts';
 import '@/ai/flows/generate-code-from-prompt.ts';
 import '@/ai/flows/generate-web-code-from-prompt.ts';
-import '@/ai/flows/simulate-code-execution.ts';
 import '@/ai/flows/execute-code-with-judge0.ts';
 import '@/ai/flows/generate-component-code-from-prompt.ts';
